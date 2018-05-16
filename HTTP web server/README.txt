6cf984acfbdbcea66535dbdb34e09322961d4b3b
Aki Puurula <puurula.aki@gmail.com>
2018-05-16 10:43:53 +0300

    Added HTTP web server sources

---
+++ HTTP web server/README.txt
@@ -0,0 +1,5 @@
+C#
+The web server was made in Visual C# and it uses the port 8080. After starting the server, the user can access the index page locally through http://localhost:8080/index/ which will serve the index.html file.
+Filling (or not filling) the form in the index page and submitting it load to a welcome page with the submitted information.
+Clicking the "request a page" button will serve a new page for the user.
+Both the index.html and test.html need to be in the same folder as the executable (for example http-server/bin/debug if debugging the solution).
---
+++ HTTP web server/src/http-server/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+
+namespace http_server
+{
+    class Program
+    {
+        private static string url = "index";
+
+        static void Main(string[] args)
+        {
+            WebServer ws = new WebServer(SendResponse, "http://localhost:8080/" + url + "/");
+            ws.Run();
+            Console.WriteLine("A simple webserver. Press a key to quit.");
+            Console.ReadKey();
+            ws.Stop();
+        }
+
+        public static string SendResponse(HttpListenerRequest request)
+        {
+
+            Console.WriteLine("Request method: " + request.HttpMethod);
+            Console.WriteLine("Content type: " + request.ContentType);
+            if (request.HttpMethod == "GET")
+            {
+                return getHandler(request);
+            }
+            else
+            {
+                return postHandler(request);
+            }
+        }
+
+        public static string getHandler(HttpListenerRequest request)
+        {
+            string content = request.QueryString["page"];
+            if (content == "" || content == null)
+            {
+                content = "index";
+            }
+            return File.ReadAllText(string.Format(@"{0}.html", content));
+        }
+
+        public static string postHandler(HttpListenerRequest request)
+        {
+            Hashtable formVars = new Hashtable();
+            string result;
+
+            if (request.HasEntityBody)
+        	{
+                using (StreamReader reader = new StreamReader(request.InputStream, request.ContentEncoding))
+                {
+                    string content = reader.ReadToEnd();
+                    content = content.Replace("\r", "");
+                    string[] pairs = content.Split('\n');
+			        for (int x = 0; x < pairs.Length; x++)
+			        {
+                        Console.WriteLine(pairs[x]);
+				        string[] item = pairs[x].Split('=');
+				        formVars.Add(item[0],System.Web.HttpUtility.UrlDecode(item[1]));
+			        }
+                }
+            }
+            result = string.Format("Welcome {0}, your email is {1}", formVars["name"], formVars["email"]);
+            Console.WriteLine(result);
+            return result;
+        }
+
+    }
+}
